296ac5db9a58dbd3f626953ce1c7d449a5a6af4b
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-06-01 18:29:54 +0100

    React Intros

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import logo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Notifications />
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" />
+          <button>OK</button>
+        </form>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
