4525eb32220c7ba242a916ccc8236791b69c12ab
elmoustafi-22 <107369052+Elmoustafi-22@users.noreply.github.com>
2024-06-09 23:13:27 +0100

    updated

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.test.js
@@ -1,61 +1,49 @@
+// task_0/dashboard/src/CourseList/CourseListRow.test.js
+
 import React from "react";
-import CourseList from "./CourseList";
-import CourseListRow from "./CourseListRow";
 import { shallow } from "enzyme";
+import CourseListRow from "./CourseListRow";
 
-const listCourses = [
-  { id: 1, name: "ES6", credit: 60 },
-  { id: 2, name: "Webpack", credit: 20 },
-  { id: 3, name: "React", credit: 40 },
-];
-
-describe("CourseList component tests", () => {
-  it("should render without crashing", () => {
-    const wrapper = shallow(<CourseList />);
-
-    expect(wrapper.exists()).toBe(true);
+describe("CourseListRow component tests", () => {
+  it("renders one cell with colspan = 2 when textSecondCell does not exist and isHeader is true", () => {
+    const wrapper = shallow(
+      <CourseListRow isHeader={true} textFirstCell="Header" />
+    );
+    expect(wrapper.find("tr").prop("style")).toHaveProperty(
+      "backgroundColor",
+      "#f5f5f5ab"
+    );
+    expect(wrapper.find("th").length).toBe(1);
+    expect(wrapper.find("th").prop("colSpan")).toBe(2);
   });
 
-  it("renders 5 different rows", () => {
-    const wrapper = shallow(<CourseList listCourses={listCourses} />);
-
-    expect(wrapper.find("thead").children()).toHaveLength(2);
-    wrapper.find("thead").forEach((node) => {
-      expect(
-        node.equals(
-          <CourseListRow
-            textFirstCell="Course name"
-            textSecondCell="Credit"
-            isHeader={true}
-          />
-        )
-      );
-    });
-
-    expect(wrapper.find("tbody").children()).toHaveLength(3);
-    expect(wrapper.find("tbody").childAt(0).html()).toEqual(
-      "<tr><td>ES6</td><td>60</td></tr>"
+  it("renders two cells when textSecondCell is present and isHeader is true", () => {
+    const wrapper = shallow(
+      <CourseListRow
+        isHeader={true}
+        textFirstCell="Header1"
+        textSecondCell="Header2"
+      />
     );
-    expect(wrapper.find("tbody").childAt(1).html()).toEqual(
-      "<tr><td>Webpack</td><td>20</td></tr>"
+    expect(wrapper.find("tr").prop("style")).toHaveProperty(
+      "backgroundColor",
+      "#f5f5f5ab"
     );
-    expect(wrapper.find("tbody").childAt(2).html()).toEqual(
-      "<tr><td>React</td><td>40</td></tr>"
-    );
+    expect(wrapper.find("th").length).toBe(2);
+    expect(wrapper.find("th").at(0).text()).toBe("Header1");
+    expect(wrapper.find("th").at(1).text()).toBe("Header2");
   });
 
-  it("renders correctely when passed a list of courses", () => {
-    const wrapper = shallow(<CourseList listCourses={listCourses} />);
-
-    expect(wrapper.find("tbody").children()).toHaveLength(3);
-    expect(wrapper.find("tbody").childAt(0).html()).toEqual(
-      "<tr><td>ES6</td><td>60</td></tr>"
+  it("renders two td elements within a tr element when isHeader is false", () => {
+    const wrapper = shallow(
+      <CourseListRow textFirstCell="Cell1" textSecondCell="Cell2" />
     );
-    expect(wrapper.find("tbody").childAt(1).html()).toEqual(
-      "<tr><td>Webpack</td><td>20</td></tr>"
+    expect(wrapper.find("tr").prop("style")).toHaveProperty(
+      "backgroundColor",
+      "#f5f5f5ab"
     );
-    expect(wrapper.find("tbody").childAt(2).html()).toEqual(
-      "<tr><td>React</td><td>40</td></tr>"
-    );
+    expect(wrapper.find("td").length).toBe(2);
+    expect(wrapper.find("td").at(0).text()).toBe("Cell1");
+    expect(wrapper.find("td").at(1).text()).toBe("Cell2");
   });
 });
